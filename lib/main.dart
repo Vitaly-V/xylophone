e190379fdd4cdbe99e81d7d1aebc7f283fcbf16e
Vitaly-V <gtopbox@gmail.com>
2019-05-14 12:41:04 +0300

    Improve UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,14 @@ class XylophoneApp extends StatelessWidget {
   Widget _buildButton(Color color, int soundNumber) {
     final player = AudioCache();
     final sound = 'note$soundNumber.wav';
-    return FlatButton(
-      child: Container(),
-      color: color,
-      onPressed: () {
-        player.play(sound);
-      },
+    return Expanded(
+      child: FlatButton(
+        child: Container(),
+        color: color,
+        onPressed: () {
+          player.play(sound);
+        },
+      ),
     );
   }
 
@@ -20,16 +22,19 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: [
-            _buildButton(Colors.red, 1),
-            _buildButton(Colors.orange, 2),
-            _buildButton(Colors.yellow, 3),
-            _buildButton(Colors.green, 4),
-            _buildButton(Colors.teal, 5),
-            _buildButton(Colors.blue, 6),
-            _buildButton(Colors.purple, 7),
-          ]),
+          child: Column(
+            children: [
+              _buildButton(Colors.red, 1),
+              _buildButton(Colors.orange, 2),
+              _buildButton(Colors.yellow, 3),
+              _buildButton(Colors.green, 4),
+              _buildButton(Colors.teal, 5),
+              _buildButton(Colors.blue, 6),
+              _buildButton(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
